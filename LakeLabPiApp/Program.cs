ff811d16871b66600eb876e2043bb8aeae2a99bb
Christoph Neuner <christoph_neuner@web.de>
2018-04-18 09:27:06 +0200

    .

--- LakeLabPiApp/Program.cs
+++ LakeLabPiApp/Program.cs
@@ -3,23 +3,28 @@ using System.Net.Http;
 using LakeLabLib;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.IO;
 
 namespace LakeLabPiApp
 {
     class Program
     {
-        //private static string uri = "http://localhost:50992/api/ReceiveValues";
-        //private static string databasePathDO = @"D:\DO.sqlite";
-        //private static string databasePathTemp = @"D:\RTD.sqlite";
+        Json
 
-        private static string uri = "http://212.227.175.108/api/ReceiveValues";
-        private static string databasePathDO = @"/home/pi/iniac/data/DO.sqlite";
-        private static string databasePathTemp = @"/home/pi/iniac/data/RTD.sqlite";
 
-        private static string deviceName = "pi1";
+        //private static string _uri = "http://localhost:50992/api/ReceiveValues";
+        //private static string _databasePathDO = @"D:\DO.sqlite";
+        //private static string _databasePathTemp = @"D:\RTD.sqlite";
+               
+        private static string _uri = "http://212.227.175.108/api/ReceiveValues";
+        private static string _databasePathDO = @"/home/pi/iniac/data/DO.sqlite";
+        private static string _databasePathTemp = @"/home/pi/iniac/data/RTD.sqlite";
 
-        private static SQLiteDbReader dbreader = new SQLiteDbReader();
-        private static HttpHelper httphelper = new HttpHelper();
+        private static string _deviceName = "pi1";
+
+        private static SQLiteDbReader _dbreader = new SQLiteDbReader();
+        private static HttpHelper _httphelper = new HttpHelper();
 
         public static async Task Main(string[] args)
         {           
@@ -27,18 +32,32 @@ namespace LakeLabPiApp
             {
                 List<ValueModel>  valueModels = new List<ValueModel>();
 
-                ValueModel modelDO = new ValueModel(deviceName, Enums.SensorTypes.Dissolved_Oxygen);               
-                modelDO.Items.AddRange(dbreader.ReadDb(databasePathDO));
-                ValueModel modelTemp = new ValueModel(deviceName, Enums.SensorTypes.Temperature);
-                modelTemp.Items.AddRange(dbreader.ReadDb(databasePathTemp));
+                ValueModel modelDO = new ValueModel(_deviceName, Enums.SensorTypes.Dissolved_Oxygen);               
+                modelDO.Items.AddRange(_dbreader.ReadDb(_databasePathDO));
+                ValueModel modelTemp = new ValueModel(_deviceName, Enums.SensorTypes.Temperature);
+                modelTemp.Items.AddRange(_dbreader.ReadDb(_databasePathTemp));
 
                 valueModels.Add(modelDO);
                 valueModels.Add(modelTemp);
 
-                string response = await httphelper.PostDataAsync(uri, valueModels);
+                string response = await _httphelper.PostDataAsync(_uri, valueModels);
                 Console.WriteLine(response);
                 System.Threading.Thread.Sleep(60*5*1000);
             }
         }
+
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader(".//parameters.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Parameters> parameters = JsonConvert.DeserializeObject<List<Parameters>>(json);
+            }
+        }
+    }
+
+    public class Parameters
+    {
+        public string
     }
 }
