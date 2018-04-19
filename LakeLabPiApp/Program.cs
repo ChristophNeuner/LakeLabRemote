c7f25a632aa8bf77ec58008180f3fc2d2c3cdca9
Christoph Neuner <christoph_neuner@web.de>
2018-04-19 21:20:24 +0200

    piapp now reading parameters from json file

--- LakeLabPiApp/Program.cs
+++ LakeLabPiApp/Program.cs
@@ -5,43 +5,72 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace LakeLabPiApp
 {
     class Program
     {
-        Parameters parameters = LoadJson()
-        
-
-        private static string _uri = 
-        private static string _databasePathDO = 
-        private static string _databasePathTemp = 
-        private static string _deviceName = 
-
         private static SQLiteDbReader _dbreader = new SQLiteDbReader();
         private static HttpHelper _httphelper = new HttpHelper();
 
         public static async Task Main(string[] args)
-        {           
+        {
             while (true)
             {
-                List<ValueModel>  valueModels = new List<ValueModel>();
+                Parameters parameters = new Parameters();
+                string databasePathDO = "";
+                string databasePathTemp = "";
 
-                ValueModel modelDO = new ValueModel(_deviceName, Enums.SensorTypes.Dissolved_Oxygen);               
-                modelDO.Items.AddRange(_dbreader.ReadDb(_databasePathDO));
-                ValueModel modelTemp = new ValueModel(_deviceName, Enums.SensorTypes.Temperature);
-                modelTemp.Items.AddRange(_dbreader.ReadDb(_databasePathTemp));
+                try
+                {
+                    if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    {
+                        parameters = await LoadJsonAsync(".\\parameters.json");
+                        databasePathDO = parameters.DatabasePathDOWindows;
+                        databasePathTemp = parameters.DatabasePathTempWindows;
+                    }
+                        
+                    else
+                    {
+                        parameters = await LoadJsonAsync(".//parameters.json");
+                        databasePathDO = parameters.DatabasePathDOLinux;
+                        databasePathTemp = parameters.DatabasePathTempLinux;
+                    }
+                    
+                    string uri = parameters.Uri;                   
+                    string deviceName = parameters.DeviceName;
+                    int sleepTime = parameters.SleepTime;
 
-                valueModels.Add(modelDO);
-                valueModels.Add(modelTemp);
+                    List<ValueModel> valueModels = new List<ValueModel>();
 
-                string response = await _httphelper.PostDataAsync(_uri, valueModels);
-                Console.WriteLine(response);
-                System.Threading.Thread.Sleep(60*5*1000);
+                    ValueModel modelDO = new ValueModel(deviceName, Enums.SensorTypes.Dissolved_Oxygen);
+                    modelDO.Items.AddRange(_dbreader.ReadDb(databasePathDO));
+                    ValueModel modelTemp = new ValueModel(deviceName, Enums.SensorTypes.Temperature);
+                    modelTemp.Items.AddRange(_dbreader.ReadDb(databasePathTemp));
+
+                    valueModels.Add(modelDO);
+                    valueModels.Add(modelTemp);
+
+                    string response = await _httphelper.PostDataAsync(uri, valueModels);
+                    Console.WriteLine(response);
+                    System.Threading.Thread.Sleep(sleepTime);
+                }
+
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine($"{e.FileName} not found");
+                    continue;
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
             }
         }
 
-        private static async Task<Parameters> LoadJson(string path)
+        private static async Task<Parameters> LoadJsonAsync(string path)
         {
             Parameters parameters = new Parameters();
             using (StreamReader r = new StreamReader(path))
@@ -59,13 +88,22 @@ namespace LakeLabPiApp
         [JsonProperty("uri")]
         public string Uri { get; set; }
 
-        [JsonProperty("databasePathDO")]
-        public string DatabasePathDO { get; set; }
+        [JsonProperty("databasePathDOWindows")]
+        public string DatabasePathDOWindows { get; set; }
 
-        [JsonProperty("databasePathTemp")]
-        public string DatabasePathTemp { get; set; }
+        [JsonProperty("databasePathTempWindows")]
+        public string DatabasePathTempWindows { get; set; }
+
+        [JsonProperty("databasePathDOLinux")]
+        public string DatabasePathDOLinux { get; set; }
+
+        [JsonProperty("databasePathTempLinux")]
+        public string DatabasePathTempLinux { get; set; }
 
         [JsonProperty("deviceName")]
         public string DeviceName { get; set; }
+
+        [JsonProperty("sleepTime")]
+        public int SleepTime { get; set; }
     }
 }
